550dcb4fd66e4245a7409ce3a4bf542e6f96008f
Jennifer Oakes <jenniferoakes@Jennifers-MBP.lan>
2017-03-21 09:27:51 -0400

    Added doc blocks for Animal function

--- src/Animal.js
+++ src/Animal.js
@@ -4,6 +4,13 @@
   window.zoo = window.zoo || {};
 
   window.zoo.Animal = class Animal {
+
+    /**
+     * Constructs a new animal
+     * @param {String} name       Name of the Animal
+     * @param {Date} dateOfBirth  Date the Animal was born
+     * @return {void} 
+     */
     constructor(name, dateOfBirth) {
       this.name = name;
       if ( ! (dateOfBirth instanceof Date) ) {
@@ -15,11 +22,16 @@
     getDob() {
       return this.dob;
     }
-
+    /**
+     * Gets the current age of the Animal in whole years
+     * @return {Number} the age of the animal
+     */
     getAge() {
       let currentDate = new Date();
       this.age = currentDate.getFullYear() - this.dob.getFullYear();
       return this.age;
+
+      //might not need variable here since I use it only one time
     }
 
     printName() {
